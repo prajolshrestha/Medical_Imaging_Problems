062e7bd4403868d0c6ba19187d1d4a7ac27f2d56
Prajol Shrestha <prajolstha9@gmail.com>
2023-08-17 17:46:58 +0200

    Add files via upload

--- 04 Deep Learning/code/main.py
+++ 04 Deep Learning/code/main.py
@@ -9,63 +9,59 @@ from AllFunctions.softMax import SoftMax
 
 
 
-
-def main():  # test_iris_data, TestNeuralNetwork
+#Test iris data
+def main():
 
     # load sample data
-    iris_data = IrisData()
+    iris_data = IrisData() # Instance of IrisData and Data class
     categories = iris_data.categories
     input_size = iris_data.feature_dim
 
-    # set network options
-    learning_rate = 1e-4  # set same value for all layers here
-    batch_size = iris_data.full_batch  # use all samples for this exercise
+    # Set network options
+    learning_rate = 1e-4 # set same value for all layers
+    batch_size = iris_data.full_batch # use all samples for this exercies
 
-    # ---------------------------
-    # Construction of the network
-    # ---------------------------
+    ################## Construction of the Network #######################
+
     net = NeuralNetwork()
 
     net.data_layer = InputLayer(iris_data)
 
-    # fully connected layer 1
+    # FC layer 1
     fcl_1 = FullyConnected(input_size, categories, batch_size, learning_rate)
     net.layers.append(fcl_1)
     net.layers.append(ReLU(categories, batch_size))
 
-    # fully connected layer 2
+    # FC layer 2
     fcl_2 = FullyConnected(categories, categories, batch_size, learning_rate)
     net.layers.append(fcl_2)
     net.layers.append(ReLU(categories, batch_size))
 
     net.loss_layer = SoftMax(categories, batch_size)
 
-    # -----------------------
-    # Training of the network
-    # -----------------------
+    ###################### Training ###########################################
+
     net.train(2000)
 
-    # ---------------------------
-    # Testing/running the network
-    # ---------------------------
-    # compute results for test data
-    data, labels = iris_data.get_test_set()
+    ####################### Testing ############################################
+
+    data,labels = iris_data.get_test_set()
     results = np.round(net.test(data))
 
-    # ---------------------------
-    # Statistics
-    # ---------------------------
-    # compute accuracy
+    ############################# Statistics #######################################
+
+    # Compute accuracy
     accuracy = compute_accuracy(results.T, labels.T)
 
-    # report the result
+    # Report the result
     if accuracy > 0.9:
         print('\nSuccess!')
     else:
         print('\nFailed! (Network\'s accuracy is below 90%)')
-    print('In this run, on the iris dataset, we achieve an accuracy of {} %'.format(str(accuracy * 100)))
+    print('In this run, on the iris dataset, we achieved an  accuracy of {} %' .format(str(accuracy*100)))
 
     net.show()
+
 
 
 def compute_accuracy(results, labels):
@@ -87,6 +83,6 @@ def compute_accuracy(results, labels):
         return correct / (correct + wrong) # Computes accuracy
 
 
-    # Check if the python script is being run as the main program or if it is being imported as a module into another script.
+# Check if the python script is being run as the main program or if it is being imported as a module into another script.
 if __name__ == '__main__':
     main()
