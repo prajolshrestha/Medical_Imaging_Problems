9c438b3d9bbd06232b3c0fa81aabcda92530bde6
Prajol Shrestha <prajolstha9@gmail.com>
2023-08-17 17:31:53 +0200

    Add files via upload

---
+++ 04 Deep Learning/code/AllFunctions/__init__.py
@@ -0,0 +1,3 @@
+__all__ = ["fullyConnected.py", "softMax.py", "reLU.py","data.py","irisData.py","inputLayer.py","neuralNetwork.py"]
+
+#list of module names that should be imported when the from module import * syntax is used
